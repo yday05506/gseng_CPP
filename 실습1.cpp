a32978b95a8cb5a1eddf86506c6b9aeb2bdff97c
yday <yangdayeon2@gmail.com>
2022-05-20 09:18:19 +0900

    초기 커밋.

---
+++ 실습1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Person 
+{
+private :
+	string sName;
+	int nAge;
+
+public:
+	Person();
+	Person(string name, int age);
+	void hi();
+};
+
+Person::Person()
+{
+	sName = "익명";
+	nAge = 0;
+}
+
+Person::Person(string name, int age)
+	:sName(name), nAge(age)
+{
+	/*sName = name;
+	nAge = age;*/
+}
+
+void Person::hi()
+{
+	cout << "이름 : " << sName << endl;
+	cout << "나이 : " << nAge << endl;
+}
+
+int main(void)
+{
+	Person p = Person();
+	p.hi();
+
+	Person* person = new Person("한승원", "100");
+	person->hi();
+
+	Person* people = new Person[5];
+	people[0].hi();
+
+	delete person;
+	delete[]people;
+}
